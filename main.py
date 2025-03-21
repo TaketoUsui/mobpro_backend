dbb64f553d13626b8b3c47d9bbc0e25e5552d15f
TaketoUsui <take20030524@yahoo.co.jp>
2025-03-21 20:08:34 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+class Message(BaseModel):
+    user: str
+    message: str
+class RoomInfo(BaseModel):
+    youtube: str
+    title: str
+    user: str
+
+app = FastAPI()
+
+@app.get("/make-room")
+async def make_room(info: RoomInfo):
+    return {
+        "status": "ok",
+        "room": "qWjlNO"
+    }
+
+@app.post("/messages/{room}")
+async def message(room: str, content: Message):
+    return {
+        "status": "ok",
+        "room": room,
+        "user": content.user,
+        "message": content.message
+    }
+
+@app.get("/messages/{room}")
+async def get_messages(room: str):
+    return {
+        "status": "ok",
+        "room": room,
+        "messages": [
+            {"user": "pigeon", "message": "hello"},
+            {"user": "giraffe", "message": "nice code"},
+            {"user": "penguin", "message": "hi"},
+            {"user": "shark", "message": "日本人いませんかー？"},
+        ]
+    }
